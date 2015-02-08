94771e0fcd64e71e3bef338e93d03883e998c41c
Knut Melvær <knut.melvaer@gmail.com>
2015-02-08 22:51:06 +0100

    bugfixes

--- index.js
+++ index.js
@@ -7,18 +7,6 @@ module.exports = {
                 if (!config.kitID) {
                     throw "Need to option 'kitID' for Typekit plugin";
                 }
-
-                if (!config.configuration) {
-                    config.configuration = 'auto';
-                }
-
-                if(typeof config.configuration === 'object' && config.configuration !== null) {
-                    configuration = JSON.stringify(config.configuration);
-                }
-                else if (['auto', 'none'].indexOf(config.configuration) != -1) {
-                    configuration = "'" + config.configuration + "'";
-                }
-
                 return "<script src='//use.typekit.net/"+config.kitID+"kij5crl.js'></script><script>try{Typekit.load();}catch(e){}</script>";
             }
         }
